fc7a3e696e4b0911cc29bfe25d5c62ae681c6c10
Chandler Childs <rubberband75@gmail.com>
2017-03-23 15:40:08 -0600

    add example to moduledoc

--- lib/double_check.ex
+++ lib/double_check.ex
@@ -14,9 +14,150 @@ defmodule DoubleCheck do
   2. Go through the list of exams, and assign each next exam to both graders in a pair
   3. Assign each remaing exam twice to the next two graders, who each have at least one batch with current smallest batch size of all batches.
 
+  ## Example
+
+  Say you have graders A, B, and C.
+  The list of unique pairs for these three would be
+
+  ```elixir
+    {'A', 'B'}, {'A', 'C'}, {'B', 'C'}
+  ```
+
+  Now lets say grader B is only part time, so they are given one batch of exams to grade,
+  whereas graders A, and C are assigned two batches each.
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |             []
+  A (batch 2) |             []
+  B           |             []
+  C (batch 1) |             []
+  C (batch 2) |             []
+
+  Next, lets say there are six exams that need grading.
+  ```elixir
+    [1,2,3,4,5,6]
+  ```
+
+
+  We first iterate trough the list of unique pairs,
+  popping exams to be assigned off the list,
+  and assigning them to both graders in the pairs,
+  and to the next batch with the least assignments.
+  > ```elixir
+  > [{'A', 'B'}, ...
+  > [1,2,3,4,5,6]
+  >  ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |            [1]
+  A (batch 2) |             []
+  B           |            [1]
+  C (batch 1) |             []
+  C (batch 2) |             []
+
+
+
+  > ```elixir
+  > ..., {'A', 'C'}, ...
+  > [1,2,3,4,5,6]
+  >    ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |            [1]
+  A (batch 2) |            [2]
+  B           |            [1]
+  C (batch 1) |            [2]
+  C (batch 2) |             []
+
+
+
+  > ```elixir
+  > ..., {'B', 'C'}]
+  > [1,2,3,4,5,6]
+  >      ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |            [1]
+  A (batch 2) |            [2]
+  B           |         [1, 3]
+  C (batch 1) |            [2]
+  C (batch 2) |            [3]
+
+
+
+  Finally, we finisih iterating across the list of exams,
+  and assign each remaining exam to the next to two graders
+  who have a batch with a current size less than or equal to every other batch.
+
+  > ```elixir
+  >   # Graders A(batch 1), and C(batch 1)
+  >   [1,2,3,4,5,6]
+  >          ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |         [1, 4]
+  A (batch 2) |            [2]
+  B           |         [1, 3]
+  C (batch 1) |         [2, 4]
+  C (batch 2) |            [3]
+
+
+
+  > ```elixir
+  >   # Graders A(batch 2), and C(batch 2)
+  >   [1,2,3,4,5,6]
+  >            ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |         [1, 4]
+  A (batch 2) |         [2, 5]
+  B           |         [1, 3]
+  C (batch 1) |         [2, 4]
+  C (batch 2) |         [3, 5]
+
+
+
+  > ```elixir
+  >   # Graders A(batch 1), and B
+  >   [1,2,3,4,5,6]
+  >              ^
+  > ```
+
+  Grader      | Assigned exams
+  :---------- | -------------:
+  A (batch 1) |      [1, 4, 6]
+  A (batch 2) |         [2, 5]
+  B           |      [1, 3, 6]
+  C (batch 1) |         [2, 4]
+  C (batch 2) |         [3, 5]
+
+
+
+  Once every exam has been assigned to a grader, the program return a list of maps, with the grader and thier assignments:
+
+  ```elixir
+  [
+    %{grader: "A", assignments: [1, 2, 4, 5, 6]},
+    %{grader: "B", assignments: [1, 3, 6]},
+    %{grader: "C", assignments: [2, 3, 4, 5]}
+  ]
+  ```
 
 
   """
+
+
 
   @doc """
   Generates a list of tuples, reperesnting each unique pair from a given list of objects.
@@ -25,8 +166,6 @@ defmodule DoubleCheck do
 
       iex> DoubleCheck.get_pairs(['A', 'B', 'C'])
       [{'A', 'B'}, {'A', 'C'}, {'B', 'C'}]
-
-
 
   """
   def get_pairs(list) do
@@ -43,6 +182,5 @@ defmodule DoubleCheck do
     {_count, _list, pairs} = r
     pairs
   end
-
 
 end
